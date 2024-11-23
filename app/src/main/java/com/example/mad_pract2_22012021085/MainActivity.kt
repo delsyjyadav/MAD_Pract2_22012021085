126565cec2dbc2c5042f8b4195c70bba0aa16610
delsyjyadav <delsyjyadav0810@gmail.com>
2024-11-23 15:11:24 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mad_pract2_22012021085/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.mad_pract2_22012021085
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val text= "Toast message"
+        val duration= Toast.LENGTH_SHORT
+        val toast = Toast.makeText(this, text, duration)
+        toast.show()
+
+
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.i("logging","onStart executing")
+        //Toast.makeText(this, "onStart", Toast.LENGTH_LONG).show()
+        Snackbar.make(findViewById(R.id.main),"Snackbar message",Snackbar.LENGTH_LONG).show()
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.i("logging","onResume executing")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.i("logging","onPause executing")
+    }
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("logging","onRestart executing")
+    }
+}
